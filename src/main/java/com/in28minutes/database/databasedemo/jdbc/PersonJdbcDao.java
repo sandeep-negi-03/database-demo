b2bdf9973a1561a61f67a95b22aff606feed3c94
Sandeep Negi <sannegi6@publicisgroupe.net>
2020-01-10 17:37:30 +0530

    Udemy Spring in Modern way Database Demo first commit

---
+++ src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -0,0 +1,31 @@
+package com.in28minutes.database.databasedemo.jdbc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.in28minutes.database.databasedemo.entity.Person;
+
+@Repository
+public class PersonJdbcDao {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	public List<Person> findAll() {
+		return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<Person>(Person.class));
+	}
+
+	public Person findbyId(int id) {
+		return jdbcTemplate.queryForObject("select * from person where id=?", new Object[] {id},
+				new BeanPropertyRowMapper<Person>(Person.class));
+	}
+
+	public int deletebyId(int id) {
+		return jdbcTemplate.update("delete from person where id=?", new Object[] {id});
+	}
+	
+}
